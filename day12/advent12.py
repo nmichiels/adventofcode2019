e6f5bd9c096639f920239118c021f56bf0ebf79f
nick.michiels <nick.michiels@uhasselt.be>
2019-12-12 13:54:19 +0100

    some tests on ODE integration

--- day12/advent12.py
+++ day12/advent12.py
@@ -94,7 +94,7 @@ def runPart1():
    print("Total energy:", getTotalEnergy(positions, velocities))
    
    
-   
+  
 def runPart2():
    positions = []
    positions.append([-19,-4,2])
@@ -171,8 +171,74 @@ def runPart2():
 
    print("Alignment after %d steps:"%timeStamp)
 
+def test():
+    from scipy.integrate import odeint
+    y0 = []
+    y0.append([-19,-4,2])
+    y0.append([-9,8,-16])
+    y0.append([-4,5,-11])
+    y0.append([1,9,-13])
+    # positions = []
+    # positions.append([-1,0,2])
+    # positions.append([2,-10,-7])
+    # positions.append([4,-8,8])
+    # positions.append([3,5,-1])
+   
+
+    y0.append([0,0,0])
+    y0.append([0,0,0])
+    y0.append([0,0,0])
+    y0.append([0,0,0])
+
+    y0 = np.asarray(y0)
+    y0 = np.reshape(y0, (np.product(y0.shape)))
+
+
+    pairs = [(0,1),(0,2),(0,3),(1,2),(1,3),(2,3)]
+    # function that returns dy/dt
+    def model(y,t):
+
+      positions = []
+      positions_dt = []
+      velocities = []
+      velocities_dt = []
+      for i in range(4):
+          positions.append([y[i*3+0],y[i*3+1],y[i*3+2]])
+          positions_dt.append([0,0,0])
+      for i in range(4, 8):
+          velocities.append([y[i*3+0],y[i*3+1],y[i*3+2]])
+          velocities_dt.append([0,0,0])
+
+      
+      # calculate new velocities
+      for pair in pairs:
+         for c in range(3):
+            if positions[pair[0]][c] < positions[pair[1]][c]:
+               velocities[pair[0]][c] += 1
+               velocities[pair[1]][c] -= 1
+               velocities[pair[0]][c] += 1
+               velocities[pair[1]][c] -= 1
+            elif positions[pair[0]][c] > positions[pair[1]][c]:
+               velocities[pair[0]][c] -= 1
+               velocities[pair[1]][c] += 1
+            else:
+               pass
+        
+      # apply velocities
+      for i in range(len(positions)):
+         for c in range(3):
+            positions[i][c] += velocities[i][c]
+
+      for i in range(4):
+          positions.append([y[i*3+0],y[i*3+1],y[i*3+2]])
+
+      return dydt
+
+    y = odeint(model,y0,1000)
+    print(y)
 
 if __name__ == '__main__':
     #freeze_support()
     # runPart1()
-	runPart2()
+	#runPart2()
+    test()
