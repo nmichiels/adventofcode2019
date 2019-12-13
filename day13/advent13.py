5063d6f7e514ac3d3d2cfcff04e5528a95196dfb
Nick Michiels <nick.michiels@uhasselt.be>
2019-12-13 16:04:53 +0100

    some better predicions, but still not working

--- day13/advent13.py
+++ day13/advent13.py
@@ -9,6 +9,12 @@ def showMap(map, score = 0, frame = 0):
     os.system('clear')  # For Linux/OS X
     print('')
     print("SCORE:", score)
+
+    for j in range(41):
+        if int(j / 10) != 0:
+            map[23,j] = j / 10
+    for j in range(41):
+        map[24,j] = j % 10
     for i in range(map.shape[0]):
         for j in range(map.shape[1]):
             if map[i,j] == 'W':
@@ -89,7 +95,35 @@ def runPart1():
 #         for col in range(map.shape[1]):
 #             print(map[row,col], end='')
 #         print('')
-      
+
+def perp( a ) :
+    b = np.empty_like(a)
+    b[0] = -a[1]
+    b[1] = a[0]
+    return b
+
+def distance(a,b):
+    return np.sqrt((a[0] - b[0])**2 + (a[1] - b[1])**2)
+
+def is_between(a,c,b):
+    return distance(a,c) + distance(c,b) == distance(a,b)
+
+def seg_intersect(a1,a2, b1,b2) :
+    da = a2-a1
+    db = b2-b1
+    dp = a1-b1
+    dap = perp(da)
+    denom = np.dot( dap, db)
+
+	
+
+    num = np.dot( dap, dp )
+    if (denom.astype(float) == 0.0):
+        return False
+
+    intersection = (num / denom.astype(float))*db + b1
+    return intersection
+
 
 def runPart2():
     instructions = np.loadtxt('input_13.txt', delimiter=',', dtype='int64')
@@ -114,7 +148,7 @@ def runPart2():
     frame = 0
     score = 0
     running = True
-    prevPosBall = (0,0)
+    prevPosBall = None#np.array([0,0])
     while True:
         while notifyOnInput.empty():
             while not notify.empty():
@@ -159,6 +193,7 @@ def runPart2():
         #     break
         if not notifyOnInput.empty():
             # print("joystick", frame)
+            
 
 
             positionBall = np.where(map == 'O')
@@ -166,27 +201,123 @@ def runPart2():
             dir = 0
             if len(positionBall[0]) > 0 and len(positionPaddle[0]) > 0:
                 
-                positionBall = (positionBall[0][0],positionBall[1][0])
-                positionPaddle = (positionPaddle[0][0],positionPaddle[1][0])
+                positionBall = np.array([positionBall[0][0],positionBall[1][0]])
+                positionPaddle = np.array([positionPaddle[0][0],positionPaddle[1][0]])
 
-                # print(positionBall, "vs", positionPaddle)
+                if prevPosBall is None:
+                    prevPosBall = positionBall
+
+                intersect = seg_intersect(positionBall, prevPosBall, np.array([positionPaddle[0],positionPaddle[1]-10]),np.array([positionPaddle[0],positionPaddle[1]+10]))
+                # print("intersect: ", intersect)
                 
-                if positionBall[1] < positionPaddle[1]:
-                    dir = -1
-                if positionBall[1] > positionPaddle[1]:
-                    dir = 1  
+                down = positionBall[0] > prevPosBall[0]
+
+   
+
+                #intersect ground
+                intersectGround = False
+                if down:
+                    intersectGround = seg_intersect(positionBall, prevPosBall, np.array([positionPaddle[0]-1,positionPaddle[1]-10]),np.array([positionPaddle[0]-1,positionPaddle[1]+10]))
+                    if intersectGround is not False and (intersectGround[1] < 0 or intersectGround[1] > 41):
+
+                        intersectLeft = seg_intersect(positionBall, prevPosBall, np.array([0,0]),np.array([positionPaddle[0],0]))
+                        if intersectLeft is not False and (intersectLeft[0] < 0 or intersectLeft[0] > positionPaddle[0]-1):
+                            intersectLeft = False
+                        else:
+                            intersectGround = seg_intersect(intersectLeft, np.array([intersectLeft[0]+1,intersectLeft[1]+1]), np.array([positionPaddle[0]-1,positionPaddle[1]-10]),np.array([positionPaddle[0]-1,positionPaddle[1]+10]))    
+
+                    
+                        intersectRight = seg_intersect(positionBall, prevPosBall, np.array([0,41]),np.array([positionPaddle[0],41]))
+                        if intersectRight is not False and (intersectRight[0] < 0 or intersectRight[0] > positionPaddle[0]-1):
+                            intersectRight = False
+                        else:
+                            intersectGround = seg_intersect(intersectRight, np.array([intersectRight[0]+1,intersectRight[1]-1]), np.array([positionPaddle[0]-1,positionPaddle[1]-10]),np.array([positionPaddle[0]-1,positionPaddle[1]+10]))    
+                
+      
+                print("intersectGround: ", intersectGround)
+
+
+                # if prevPosBall[0] == positionBall[0] and prevPosBall[1] == positionBall[1]:
+                #     pass
+                # else:
+                #     dir = positionBall - prevPosBall
+                #     trackedPos = positionBall
+                #     while trackedPos[0] != positionPaddle[0]:
+                #         newPos = trackedPos + dir
+                #         if map[newPos[0],newPos[1]] == 'X':
+                #             print("hit a wall")
+                #             dir = -dir
+                #             newPos = trackedPos + dir
+
+                # check if hit on paddle
+                if positionBall[0] + 1 == positionPaddle[0] and positionBall[1] == positionPaddle[1]:
+                    intersectGround = False
+
+                    # direction = positionBall - prevPosBall
+                    # if direction[1] > 0:
+                    #     dir = -1
+                    # elif direction[1] < 0:
+                    #     dir = 1
+                    # else:
+                    #     dir = 0
+
+        
+                if intersectGround is not False:
+                    if intersectGround[1] < positionPaddle[1]:
+                        dir = -1
+                    elif intersectGround[1] > positionPaddle[1]:
+                        dir = 1  
+                    else:
+                        if positionBall[1] > prevPosBall[1]:
+                            dir = 1  
+                        elif positionBall[1] < prevPosBall[1]:
+                            dir = -1  
+                        else:
+                            dir = 0
+
+                else:
+                    if positionBall[1] < positionPaddle[1]:
+                        dir = -1
+                    elif positionBall[1] > positionPaddle[1]:
+                        dir = 1
+                    # else:
+                    #     if positionBall[1] > prevPosBall[1]:
+                    #         dir = 1  
+                    #     elif positionBall[1] < prevPosBall[1]:
+                    #         dir = -1  
+                    #     else:
+                    #         dir = 0
+                # else:
+                #     # predict dir
+                #     if positionBall[1] > prevPosBall[1]:
+                #         dir = 1  
+                #     if positionBall[1] < prevPosBall[1]:
+                #         dir = -1
+
+
+                # if down and intersect is not False:
+                #     if intersect[1] < positionPaddle[1]:
+                #         dir = -1
+                #     if intersect[1] > positionPaddle[1]:
+                #         dir = 1  
+                # else:
+                #     if positionBall[1] < positionPaddle[1]:
+                #         dir = -1
+                #     if positionBall[1] > positionPaddle[1]:
+                #         dir = 1  
 
                 prevPosBall = positionBall
       
             notifyOnInput.get()
-            time.sleep(0.0)
+            
+            # time.sleep(0.1)
             inputs.put(dir)
             
             frame += 1
     
        
     print("end")
-    showMap(map, score, frame)  
+    # showMap(map, score, frame)  
     # part 1
     unique, counts = np.unique(map, return_counts=True)
     occ = dict(zip(unique, counts))
